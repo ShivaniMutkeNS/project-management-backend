0c8043eace31c08a7c79ed7a47ac67c44f71c507
shivanimutkeNS <shivani.mutke@noovosoft.com>
2024-05-12 12:58:11 +0530

    enable env file

---
+++ src/main/java/com/example/pm/load.java
@@ -0,0 +1,10 @@
+//package com.example.pm;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//
+//public class EnvLoader {
+//    public static void load() {
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//    }
+//}
